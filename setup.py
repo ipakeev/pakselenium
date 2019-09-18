1106f597e1ae5104f2261e0a1cff5210ef1438d6
Ipakeev <ipakeev93@gmail.com>
2019-09-18 10:13:03 +0300

    0.0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pakselenium',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/ipakeev',
+    license='MIT',
+    author='Ipakeev',
+    author_email='ipakeev93@gmail.com',
+    description='Selenium Wrapper'
+)
