1b4aa3d3fde6b6e9456408d9ba12e7fb93af687e
Ipakeev <ipakeev93@gmail.com>
2020-08-26 19:47:42 +0300

    0.3.5 fix

--- pakselenium/__init__.py
+++ pakselenium/__init__.py
@@ -2,5 +2,5 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 from .browser import Browser, PageElement, Selector
-from .helpers import close_popup, wait_if_error_page, call_if_exception
+from .helpers import close_popup, call_if_exception
 from .utils import expected_conditions as EC
